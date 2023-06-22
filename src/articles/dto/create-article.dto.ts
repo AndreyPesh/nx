f6434f90b5373d8a558472303fbc369692091ab9
Andrey Peshko <coursestud@mail.ru>
2023-06-22 22:19:18 +0300

    feat: add pipi

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,12 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description?: string;
 
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
